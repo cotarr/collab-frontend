b12b75a120fa2f40e517b345928eb7fe4c0a062e
cotarr <cotarr@yahoo.com>
2021-08-28 14:26:24 -0500

    Change token/revoke to POST request

--- server/auth/logout.js
+++ server/auth/logout.js
@@ -5,53 +5,43 @@ const config = require('../config');
 
 // ----------------------------------------------------------
 // The /logout route
-//   1) Request revoke access-token
-//   2) Request revoke refresh-token
+//   1) Request revoke access-token and refresh-token in same request
 //   3) Logout of local passport session
 //   4) Redirect to backend /logout to invalidate cookie from auth server
 // ----------------------------------------------------------
 module.exports = (req, res, next) => {
   if (('authInfo' in req) && ('access_token' in req.authInfo)) {
-    console.log('revoking token');
-    const fetchUrl1 = config.oauth2.authURL + '/api/revoke?token=' + req.authInfo.access_token;
-    const fetchUrl2 = config.oauth2.authURL + '/api/revoke?token=' + req.authInfo.refresh_token;
+    const fetchUrl = config.oauth2.authURL + '/token/revoke';
+
+    const body = {
+      access_token: req.authInfo.access_token
+    };
+    if ('refresh_token' in req.authInfo) {
+      body.refresh_token = req.authInfo.refresh_token;
+    }
+
     const fetchOptions = {
-      method: 'GET',
+      method: 'POST',
       timeout: 5000,
       headers: {
+        'Content-Type': 'application/json',
         Accept: 'application/json'
-      }
+      },
+      body: JSON.stringify(body)
     };
     //
-    // Revoke access-token
+    // Revoke access-token and refresh_token
     //
-    fetch(fetchUrl1, fetchOptions)
+    fetch(fetchUrl, fetchOptions)
       .then((response) => {
         if (response.ok) {
           return response.json();
         } else {
           throw new Error('Fetch status ' + response.status + ' ' +
-          fetchOptions.method + ' ' + fetchUrl1);
+          fetchOptions.method + ' ' + fetchUrl);
         }
       })
-      //
-      // Revoke refresh token
-      //
-      .then((json) => {
-        return fetch(fetchUrl2, fetchOptions);
-      })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Fetch status ' + response.status + ' ' +
-          fetchOptions.method + ' ' + fetchUrl2);
-        }
-      })
-      //
-      // Rest is logout local and remote
-      //
-      .then((json) => {
+      .then(() => {
         req.logout();
         return res.redirect('/loggedout.html');
       })
