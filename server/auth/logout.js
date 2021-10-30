eb5c9bb9c1d05a721ba752e4f8559b3be6505951
cotarr <cotarr@yahoo.com>
2021-10-30 12:32:30 -0500

    minor html

--- server/auth/logout.js
+++ server/auth/logout.js
@@ -22,10 +22,9 @@ const config = require('../config');
 /**
  * Local server logout route handler, use passport logout() method to clear user from session
  */
-
 const logoutHtml =
   logout1Html +
-  'You have been successfully logged out of the frontend-collab web server at ' +
+  'You have been successfully logged out of the web server at ' +
    config.oauth2.mainURL + '. ' +
   'You may still be logged in to the authorization server at ' + config.oauth2.authURL + '. ' +
   'This may allow automatic login upon returning to this site. ' +
